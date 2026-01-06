6ce2c1cdebe88ec9e894b032d27570fc13bc344b
Marcin <marcinm.iwan@gmail.com>
2026-01-06 13:13:22 +0100

    added App class

--- src/main/java/com/example/demo/client/ClientGUI.java
+++ src/main/java/com/example/demo/client/ClientGUI.java
@@ -1,9 +1,15 @@
 package com.example.demo.client;
 
 import com.example.demo.Message;
-
+import javax.swing.*;
 import java.util.concurrent.ExecutionException;
 
-public class ClientGUI {
+public class ClientGUI extends JFrame {
+    public ClientGUI(String username){
+        super("User: " + username);
 
+        setSize(1280, 720);
+        setLocationRelativeTo(null);
+
+    }
 }
