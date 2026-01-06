94be8789f81059e2f59ff2a8612d8540df9439d7
Marcin <marcinm.iwan@gmail.com>
2026-01-06 13:04:25 +0100

    remove old ClientGUI config

--- src/main/java/com/example/demo/client/ClientGUI.java
+++ src/main/java/com/example/demo/client/ClientGUI.java
@@ -5,12 +5,5 @@ import com.example.demo.Message;
 import java.util.concurrent.ExecutionException;
 
 public class ClientGUI {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        ChatStompClient chatStompClient = new ChatStompClient("LubieJave");
-        chatStompClient.sendMessage(new Message("Hello from LubieJave!", "LubieJave"));
-        chatStompClient.disconnectUser("LubieJave");
 
-        Thread.sleep(2000); // for debugging
-
-    }
 }
